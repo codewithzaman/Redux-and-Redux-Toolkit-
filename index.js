7efc5999bcd0f83f0f044d915e4fd8bc92c64599
codewithzaman <naderzaman50@gmail.com>
2024-01-03 00:18:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log('from index.js');
+const redux = require('redux');
+const createStore = redux.createStore
 // Actions the only way application can interact with store.
 // carry some information form application to the store.
 // plain jS objects.
@@ -18,7 +19,7 @@ function orderCake(){
 // (previousState,action) => newState
 const initialState = {
     numOfCakes :10,
-    anotherProperty:0
+    // anotherProperty:0
 }
 const reducer =(state=initialState,action)=>{
     switch (action.type) {
@@ -34,3 +35,16 @@ const reducer =(state=initialState,action)=>{
     }
 }
 // Redux Store 
+// one store for entire application 
+// Responsibilities : holds application state 
+// Allows access to state by getState(),
+// Allows state to update by dispatch(action)
+// registers listners via subscribe(listener)
+const store = createStore(reducer)
+console.log('initial State', store.getState());
+ const unsubscribe = store.subscribe(()=>console.log('updated State' , store.getState()));
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+unsubscribe();
+
