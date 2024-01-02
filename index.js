ca41c6b3003a6be4acbfec1506ed66bd84eea24f
codewithzaman <naderzaman50@gmail.com>
2024-01-03 00:01:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function orderCake(){
     } 
 }
 // Reducers : It specify how apps state changes in response to actions sent to the store.
+// Reducer is a function that accepts state and action as arguments and returns next state 
+// of the application.
+// (previousState,action) => newState
+const initialState = {
+    numOfCakes :10
+}
+const reducer =(state=initialState,action)=>{
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return{
+                ...state,
+                numOfCakes:state.numOfCakes-1
+            }
+            
+        default:
+            return state
+            
+    }
+}
