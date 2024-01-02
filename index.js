2896fc6c6482b06fbe07241bd42f66c48d14ffbb
codewithzaman <naderzaman50@gmail.com>
2024-01-02 23:45:37 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('from index.js');
+// Actions the only way application can interact with store.
+// carry some information form application to the store.
+// plain jS objects.
+// have a 'type' property that describes something that happend in the application.
+// type property defined as string constants.
+// Example of Cake Shop Actions
+const CAKE_ORDERED = 'CAKE_ORDERED';
+function orderCake(){
+    return {
+        type:CAKE_ORDERED,
+        quantity: 1,
+    } 
+}
